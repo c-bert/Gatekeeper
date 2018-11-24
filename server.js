87f657efa71e12433457686084b251ef38e749a3
c-bert <danceher76@gmail.com>
2018-11-24 15:14:31 -0600

    final version

--- server.js
+++ server.js
@@ -72,9 +72,8 @@ function gateKeeper(req, res, next) {
   });
   next();
 }
-
 // Add the middleware to your app!
-
+app.use(gateKeeper);
 // this endpoint returns a json object representing the user making the request,
 // IF they supply valid user credentials. This endpoint assumes that `gateKeeper` 
 // adds the user object to the request if valid credentials were supplied.
